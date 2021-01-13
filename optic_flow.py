632d68c93b2de20510f6d82c0f115f211af2d423
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-01-14 03:51:41 +0800

    Update optic_flow.py

--- optic_flow.py
+++ optic_flow.py
@@ -8,10 +8,7 @@ from cv2 import normalize, NORM_MINMAX
 def optic_flow(img_curr, img_next):
     gray_curr = cvtColor(img_curr, COLOR_RGB2GRAY)
     gray_next = cvtColor(img_next, COLOR_RGB2GRAY)
-
-    hsv = np.zeros((100, 200, 3))
-    hsv[:,:,1] = cvtColor(img_next, COLOR_RGB2HSV)[:,:,1]
-
+   
     flow = calc(gray_curr,
                 gray_next,
                 None,
@@ -23,10 +20,14 @@ def optic_flow(img_curr, img_next):
                 1.3,
                 0)
 
+    # magnitude, angle
     mag, ang = cartToPolar(flow[..., 0], flow[..., 1])
+
+    hsv = np.zeros((100, 200, 3))
     hsv[:,:,0] = ang * (180 / np.pi / 2)
-    hsv[:,:,2] = normalize(mag,None,0,255,NORM_MINMAX)
-    hsv = np.asarray(hsv, dtype= np.float32)
-    rgb_flow = cvtColor(hsv,COLOR_HSV2RGB)
+    hsv[:,:,1] = cvtColor(img_next, COLOR_RGB2HSV)[:,:,1]
+    hsv[:,:,2] = normalize(mag, None, 0, 255, NORM_MINMAX)
+    hsv = np.asarray(hsv, dtype = np.float32)
+    rgb_flow = cvtColor(hsv, COLOR_HSV2RGB)
 
     return rgb_flow
