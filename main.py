daa3662c11c2db5a4c4d231442660a74f56a9118
root <llgxvi@gmail.com>
2021-01-14 01:09:03 +0800

    a

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from model import make_model
+from model import model as make_model
+from optic_flow import optic_flow
 import numpy as np
 
 speed = np.loadtxt("train.txt")
+frame = 20400
 
-x = 
+input = np.zeros((100,))
+output = np.zeros((100,))
+
+n = 0
+j = 0
+while(n < 199):
+    img1 = cv2.imread("stretched/%d.jpg" % n)
+    img2 = cv2.imread("stretched/%d.jpg" % n+1)
+    prev = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+    next = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+    diff = optic_flow(prev, next)
+    input[j] = diff
+    output[j] = np.mean(speed[n], speed[n+1])
 
 model = make_model()
 
-model.fit(x=None,
-          y=None,
+model.fit(x=input,
+          y=output,
           batch_size=None,
           epochs=1,
           verbose=1)
