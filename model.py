74467466b9b8fd6767b6c4e4bc720cc813bb7836
root <llgxvi@gmail.com>
2021-01-14 13:33:34 +0800

    a

--- model.py
+++ model.py
@@ -8,47 +8,47 @@ def model():
 
     m.add(Conv2D(24, (5,5),
                  strides=(2,2),
-                 padding = 'valid',
-                 kernel_initializer = 'he_normal',
-                 name = 'conv1'))
+                 padding='valid',
+                 kernel_initializer='he_normal',
+                 name='conv1'))
     m.add(ELU())
     m.add(Conv2D(36, (5,5),
                  strides=(2,2),
-                 padding = 'valid',
-                 kernel_initializer = 'he_normal',
-                 name = 'conv2'))
+                 padding='valid',
+                 kernel_initializer='he_normal',
+                 name='conv2'))
     m.add(ELU())
     m.add(Conv2D(48, (5,5),
                  strides=(2,2),
-                 padding = 'valid',
-                 kernel_initializer = 'he_normal',
-                 name = 'conv3'))
+                 padding='valid',
+                 kernel_initializer='he_normal',
+                 name='conv3'))
     m.add(ELU())
 
     m.add(Dropout(0.5))
 
     m.add(Conv2D(64, (3,3),
-                 strides = (1,1),
-                 padding = 'valid',
-                 kernel_initializer = 'he_normal',
-                 name = 'conv4'))
+                 strides=(1,1),
+                 padding='valid',
+                 kernel_initializer='he_normal',
+                 name='conv4'))
     m.add(ELU())
     m.add(Conv2D(64, (3,3),
                  strides= (1,1),
-                 padding = 'valid',
-                 kernel_initializer = 'he_normal',
-                 name = 'conv5'))
+                 padding='valid',
+                 kernel_initializer='he_normal',
+                 name='conv5'))
 
-    m.add(Flatten(name = 'flatten'))
+    m.add(Flatten(name='flatten'))
     m.add(ELU())
 
-    m.add(Dense(100, kernel_initializer = 'he_normal', name = 'fc1'))
+    m.add(Dense(100, kernel_initializer='he_normal', name='fc1'))
     m.add(ELU())
-    m.add(Dense(50,  kernel_initializer = 'he_normal', name = 'fc2'))
+    m.add(Dense(50,  kernel_initializer='he_normal', name='fc2'))
     m.add(ELU())
-    m.add(Dense(10,  kernel_initializer = 'he_normal', name = 'fc3'))
+    m.add(Dense(10,  kernel_initializer='he_normal', name='fc3'))
     m.add(ELU())
-    m.add(Dense(1,   kernel_initializer = 'he_normal', name='output'))
+    m.add(Dense(1,   kernel_initializer='he_normal', name='output'))
 
     return m
 
