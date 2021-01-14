af5366446ec43eccc846475ca18e662ff6832c38
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-01-14 17:33:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,29 +4,34 @@ import numpy as np
 import cv2
 from keras.optimizers import Adam
 from keras.models import load_model
+import math
 
 speed = np.loadtxt('train.txt')
 frame_train = 20400
 frame_test = 10798
 
-frame_train = 1000
-frame_test = 100
+batch_size = 32
+batch = math.floor(frame_train / batch_size)
 
 h, w, _ = cv2.imread('frame_train/0.jpg').shape
-input = np.zeros((frame_train - 1, h, w, 3), dtype='float16')
-output = np.zeros((frame_train - 1))
 
-input_test = np.zeros((frame_test - 1, h, w, 3), dtype='float16')
+generator_train():
+    input = np.zeros((batch_size, h, w, 3), dtype='float16')
+    output = np.zeros((batch_size))
 
-for i in range(frame_train - 1):
-    img1 = cv2.imread('frame_train/%d.jpg' % i)
-    img2 = cv2.imread('frame_train/%d.jpg' % (i+1))
-    curr = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
-    next = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
-    diff = optic_flow(curr, next)
+    n = 0
+    while True:
+        for i in range(batch_size * n, batch_size):
+            img1 = cv2.imread('frame_train/%d.jpg' % i)
+            img2 = cv2.imread('frame_train/%d.jpg' % (i+1))
+            curr = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+            next = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+            diff = optic_flow(curr, next)
 
-    input[i] = diff
-    output[i] = np.mean([speed[i], speed[i+1]])
+        input[i] = diff
+        output[i] = np.mean([speed[i], speed[i+1]])
+        yield input, output
+        n += 1
 
 adam = Adam(0.0001,
             beta_1=0.9,
@@ -38,9 +43,7 @@ model = make_model()
 model.compile(optimizer=adam,
               loss='mse')
 
-model.fit(x=input,
-          y=output,
-          batch_size=2,
+model.fit(train_generator,
           epochs=100,
           verbose=2)
 
