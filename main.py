85836f0bf2fd5eb954ba822a762c383096197abc
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-01-14 14:26:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ for i in range(frame-1):
 print('Shape of input, output')
 print(input.shape, output.shape)
 
-adam = Adam(lr=1e-4,
+adam = Adam(lr=0.01,
             beta_1=0.9,
             beta_2=0.999,
             epsilon=1e-08,
