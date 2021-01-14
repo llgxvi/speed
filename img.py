8e953cc91ccc6ba03b1c9339df1c0420393a35b3
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-01-14 13:08:03 +0800

    Update img.py

--- img.py
+++ img.py
@@ -1,34 +1,22 @@
 import cv2
 
-'''
-extract video frame, save as jpg
-h,w
-480,640
-100,150 cropped
-100,200 stretched
-'''
-
 cap = cv2.VideoCapture('train.mp4')
 frameC = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 frameW = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frameH = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-c = 0
-ret = True
-while (c < frameC and ret):
+for c in range (frameC):
     ret, img = cap.read()
 
-    # frame
-    cv2.imwrite("train/%d.jpg" % c, img)
+    if ret is False:
+        print('Fail at frame', c)
+        break
 
-    # cropped
-    img = img[189:189+100, 244:244+150, :]
-    cv2.imwrite("cropped/%d.jpg" % c, img)
-
-    # stretched
-    img = cv2.resize(img, dsize=(200, 100), interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite("stretched/%d.jpg" % c, img)
-
-    c += 1
+    img = img[100:-100, :, :]
+    cv2.imwrite('frame/%d.jpg' % c, img)
 
 cap.release()
+
+if __name__ == '__main__':
+    im = cv2.imread('frame/0.jpg')
+    print(im.shape, im.dtype, type(im))
