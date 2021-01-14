2ff25a16776ab8e456101a58b5b5e047d6d04bb4
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-01-14 15:28:09 +0800

    Update img.py

--- img.py
+++ img.py
@@ -1,6 +1,7 @@
 import cv2
+import sys
 
-cap = cv2.VideoCapture('train.mp4')
+cap = cv2.VideoCapture(sys.argv[1] + '.mp4')
 frameC = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 frameW = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frameH = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -13,10 +14,10 @@ for c in range (frameC):
         break
 
     img = img[150:-150, 200:-200, :]
-    cv2.imwrite('frame/%d.jpg' % c, img)
+    cv2.imwrite('frame_%s/%d.jpg' % (sys.argv[1], c), img)
 
 cap.release()
 
 if __name__ == '__main__':
-    im = cv2.imread('frame/0.jpg')
+    im = cv2.imread('frame_train/0.jpg')
     print(im.shape, im.dtype, type(im))
