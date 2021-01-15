e026dfd6053f9784db8ae1d4ce8082f4e72f4bb3
root <llgxvi@gmail.com>
2021-01-15 14:30:14 +0800

    a

--- fit.py
+++ fit.py
@@ -5,46 +5,45 @@ import numpy as np
 import cv2
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
-import math
 from sklearn.utils import shuffle
+from imread import imread
 
 X_label = np.loadtxt('train.txt')
-X_frames = 20400
+X_size = X_label.shape[0]
 
 batch_size = 64
-batch = X_frames // batch_size
+batch = X_size // batch_size
 
-h, w, _ = cv2.imread('frame_train/0.jpg').shape
+h, w, _ = imread(0).shape
 
-def generator_train():
-    input = np.zeros((batch_size, h, w, 3), dtype=np.float32)
-    output = np.zeros((batch_size))
+def generator_x():
+    x = np.zeros((batch_size, h, w, 3))
+    y = np.zeros((batch_size))
 
-    n = 0
+    b = 0
     while True:
         bright_factor = 0.2 + np.random.uniform()
-        j = 0
-        for i in range(batch_size * n, batch_size * n + batch_size):
-            img1 = cv2.imread('frame_train/%d.jpg' % i)
-            img2 = cv2.imread('frame_train/%d.jpg' % (i+1))
-            curr = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
-            next = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
 
-            #curr = change_brightness(curr, bright_factor)
-            #next = change_brightness(next, bright_factor)
+        i = 0
+        for j in range(batch_size * b, batch_size * (b + 1)):
+            curr = imread(j)
+            next = imread(j + 1)
+
+            curr = change_brightness(curr, bright_factor)
+            next = change_brightness(next, bright_factor)
 
             diff = optic_flow(curr, next)
-            s1 = X_label[i]
-            s2 = X_label[i+1]
 
-        input[j] = diff
-        output[j] = np.mean([s1, s2])
-        j += 1
-        input, output = shuffle(input, output)
-        yield (input/256-.5, output)
-        n += 1
-        if n == batch:
-            n = 0
+            x[i] = diff
+            y[i] = np.mean(X_label[j:j+1])
+            i += 1
+
+        x, y = shuffle(x, y)
+        yield (x/256 - 0.5, y)
+
+        b += 1
+        if b == batch:
+            b = 0
 
 import sys
 adam = Adam(float(sys.argv[1]),
@@ -53,7 +52,7 @@ adam = Adam(float(sys.argv[1]),
             epsilon=1e-08)
 
 es = EarlyStopping(monitor='loss',
-                   min_delta=1e-4,
+                   min_delta=1e-3,
                    patience=1)
 
 model = make_model((h, w, 3))
@@ -61,10 +60,10 @@ model = make_model((h, w, 3))
 model.compile(optimizer=adam,
               loss='mse')
 
-model.fit(generator_train(),
+model.fit(generator_x(),
           batch_size=batch_size,
           epochs=100,
-          steps_per_epoch=X_frames//batch_size,
+          steps_per_epoch=X_size // batch_size,
           callbacks=[es],
           verbose=1)
 
---
+++ imread.py
@@ -0,0 +1,6 @@
+import cv2
+
+def imread(index):
+    img = cv2.imread('train_frames/' + index + '.jpg')
+    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return rgb
