291f8852c5882699de8ba2080aa618f515527723
root <llgxvi@gmail.com>
2021-01-18 21:27:49 +0800

    🍺

--- progress.py
+++ progress.py
@@ -4,6 +4,7 @@ def progress(current, total, barLength = 20):
     spaces  = '.' * (barLength - len(arrow))
 
     if percent == 100:
-        print('Progress: [=%s%s] %f%%' % (arrow, spaces, percent), end='\n')
+        arrow = arrow[:-1] + '='
+        print('Progress: [=%s%s] %.0f%%   ' % (arrow, spaces, percent), end='\n')
     else:
         print('Progress: [=%s%s] %.2f%%' % (arrow, spaces, percent), end='\r')
