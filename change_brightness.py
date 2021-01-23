edf979f468c06fa4bc64adadf321ceb1d68cddfb
root <llgxvi@gmail.com>
2021-01-23 18:56:06 +0800

    🍺

--- change_brightness.py
+++ change_brightness.py
@@ -1,10 +1,15 @@
-import cv2
+import cv2 as cv
 
-# rgb: rgb img
-# bf:  bright factor
-def change_brightness(rgb, bf):
-    hsv = cv2.cvtColor(rgb, cv2.COLOR_RGB2HSV)
-    hsv[:,:,2] = hsv[:,:,2] * bf
+def change_brightness(img, bf):
+    '''
+    img:    bgr
+    bf:     0~1
+    return: bgr
+    '''
 
-    rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-    return rgb
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    hsv[..., 2] = hsv[..., 2] * bf
+
+    bgr  = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+
+    return bgr
